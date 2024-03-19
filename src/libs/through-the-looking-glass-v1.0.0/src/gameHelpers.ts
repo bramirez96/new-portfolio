6d8a633a23b75d311a1346ab895916ddd8e14f70
bramirez96 <bran.ramirez.don@gmail.com>
2024-03-19 13:05:13 -0700

    New helper function for tagged template literals. Definitely not the easiest way to do this but I love how these functions work

--- src/libs/through-the-looking-glass-v1.0.0/src/gameHelpers.ts
+++ src/libs/through-the-looking-glass-v1.0.0/src/gameHelpers.ts
@@ -1,9 +1,23 @@
 // ! Copyright (c) 2024, Brandon Ramirez, brr.dev
 
 /**
- * This function correctly formats the input tags for interactive elements, to ensure
- * that we're formatting them consistently for the best Player experience.
+ * Format the input string as an interactive tag. Instead of calling this directly,
+ * it's recommended to use the `tag` method below!
  */
 export function tag(inputTag: string): string {
-    return `>${inputTag}<`;
+    return `>${inputTag}<`.toLocaleLowerCase();
+}
+
+/**
+ * Allows easily formatting input tag strings in a template literal.
+ * @example tag`To the ${'north'} is your living room.`
+ */
+export function tagged(strings: TemplateStringsArray, ...tags: unknown[]) {
+    let tagString = "";
+
+    for (const i in tags) {
+        tagString += strings[i] + tag(`${tags[i]}`);
+    }
+
+    return tagString + strings.slice(-1);
 }
