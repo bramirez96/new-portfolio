54aa0d6e75dacd9ceaed56c081d24d2a74dee4e5
bramirez96 <bran.ramirez.don@gmail.com>
2024-03-20 09:59:17 -0700

    New helper module to put stuff related to keyboard events

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,9 @@
 // ! Copyright (c) 2024, Brandon Ramirez, brr.dev
 
-export { default as createRipple } from './createRipple';
-export { default as useFocusTracker } from './useFocusTracker';
-export { default as useFocusAutoscroll } from './useFocusAutoscroll';
-export { default as useEmptyTracker } from './useEmptyTracker';
-export { default as useMediaQueries } from './useMediaQueries';
-export * from './DarkModeContext';
+export { default as createRipple } from "./createRipple";
+export { default as useFocusTracker } from "./useFocusTracker";
+export { default as useFocusAutoscroll } from "./useFocusAutoscroll";
+export { default as useEmptyTracker } from "./useEmptyTracker";
+export { default as useMediaQueries } from "./useMediaQueries";
+export * from "./DarkModeContext";
+export { default as KeyEventHelpers } from "./KeyEventHelpers";
