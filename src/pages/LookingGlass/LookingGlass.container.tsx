1bcb15a900e2c1d5546be3015ca3394194130893
bramirez96 <bran.ramirez.don@gmail.com>
2024-03-20 11:45:52 -0700

    The looking glass game page now correctly creates a ConsoleController, and can properly link into it! Now we need to switch back to the game engine, after some small UI tweaks to the console component itself

--- src/pages/LookingGlass/LookingGlass.container.tsx
+++ src/pages/LookingGlass/LookingGlass.container.tsx
@@ -5,10 +5,31 @@ import { Console } from "../../components/organisms";
 import { classnames } from "../../libs";
 
 import "./LookingGlass.scss";
+import { ConsoleController } from "../../utils";
+import { ReactNode, useCallback, useEffect, useState } from "react";
 
 export interface LookingGlassProps {}
 
 export default function LookingGlassContainer(props: LookingGlassProps) {
+    const [consoleController, setConsoleController] = useState<ConsoleController<ReactNode>>();
+
+    // Create a callback to build the ConsoleController and save it in state
+    const buildConsoleController = useCallback(
+        (params: ConstructorParameters<typeof ConsoleController<ReactNode>>[0]) => {
+            setConsoleController(new ConsoleController(params));
+        },
+        []
+    );
+
+    // Once the console controller is built, we can then build the GameController
+    useEffect(() => {
+        // // !TEST - OMG IT WORKS
+        if (consoleController) {
+            // consoleController.updatePrefix(<>TEST PREFIX {">>"}&nbsp;</>);
+            // consoleController.clearOutput();
+        }
+    }, [consoleController]);
+
     return (
         <HomeTemplate>
             <div className={classnames("ui-looking-glass-wrapper")}>
@@ -16,6 +37,7 @@ export default function LookingGlassContainer(props: LookingGlassProps) {
                     onInputSubmit={(text) => {
                         alert(`Submitted: ${text}`);
                     }}
+                    buildConsoleController={buildConsoleController}
                 />
             </div>
         </HomeTemplate>
