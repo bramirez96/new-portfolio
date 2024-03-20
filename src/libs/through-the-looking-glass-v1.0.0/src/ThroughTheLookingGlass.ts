cb1d0712e889d039a5f031a8ed383c0418cd0e83
bramirez96 <bran.ramirez.don@gmail.com>
2024-03-20 14:49:24 -0700

    Pretty contrived game disc object, will require some cleanup later as it's kind of redundant

---
+++ src/libs/through-the-looking-glass-v1.0.0/src/ThroughTheLookingGlass.ts
@@ -0,0 +1,8 @@
+// ! Copyright (c) 2024, Brandon Ramirez, brr.dev
+
+import loadZone from "./loadZone";
+import { GameDiscDefinition } from "./gameTypes";
+
+export default {
+    loadZone,
+} as GameDiscDefinition;
