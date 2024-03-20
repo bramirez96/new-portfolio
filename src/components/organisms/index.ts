386a5daabc90055370262744f71f00a15092422b
bramirez96 <bran.ramirez.don@gmail.com>
2024-03-20 09:30:05 -0700

    Console component CSS container updates

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,4 +1,5 @@
 // ! Copyright (c) 2024, Brandon Ramirez, brr.dev
 
-export * from './Header';
-export * from './Hero';
+export * from "./Header";
+export * from "./Hero";
+export * from "./Console";
