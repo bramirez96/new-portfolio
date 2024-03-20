92f2132085866148fc7d5486b031506ef6336703
bramirez96 <bran.ramirez.don@gmail.com>
2024-03-20 14:48:16 -0700

    Define a type for game discs in an effort to generalize more handling

--- src/libs/through-the-looking-glass-v1.0.0/src/gameTypes.ts
+++ src/libs/through-the-looking-glass-v1.0.0/src/gameTypes.ts
@@ -2,15 +2,19 @@
 
 import GameController from "./GameController";
 import Player from "./classes/Player";
-import Zone from "./classes/Zone";
+import Zone, { ZoneDefinition } from "./classes/Zone";
+import { ReactNode } from "react";
+import { ZoneID } from "../../through-the-looking-glass-v0.1.0/src/GameController";
 
 /**
  * Every callback, no matter what it relates to, could possibly require access
  * to the Game, Player, and Zone objects. This object should get passed as the
  * final argument to every game callback we define.
+ *
+ * TODO remove IOType default eventually, and pass it along as needed
  */
-export type GameCallbackState = {
-    game: GameController;
+export type GameCallbackState<IOType = ReactNode> = {
+    game: GameController<IOType>;
     zone: Zone;
     player: Player;
 };
@@ -20,3 +24,10 @@ export type GameCallbackState = {
  * and mapping them to boolean values indicating the state of the condition.
  */
 export type ConditionMap = Record<string, boolean>;
+
+/**
+ * This is effectively how we will
+ */
+export type GameDiscDefinition = {
+    loadZone: (zoneID?: ZoneID) => Promise<ZoneDefinition>;
+};
