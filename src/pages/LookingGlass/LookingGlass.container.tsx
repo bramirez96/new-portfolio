fbc1e0f068505ebbf29a3bc200546369a449659e
bramirez96 <bran.ramirez.don@gmail.com>
2024-03-20 09:29:35 -0700

    New page component for the game

---
+++ src/pages/LookingGlass/LookingGlass.container.tsx
@@ -0,0 +1,19 @@
+// ! Copyright (c) 2024, Brandon Ramirez, brr.dev
+
+import { HomeTemplate } from "../../components/templates";
+import { Console } from "../../components/organisms";
+import { classnames } from "../../libs";
+
+import "./LookingGlass.scss";
+
+export interface LookingGlassProps {}
+
+export default function LookingGlassContainer(props: LookingGlassProps) {
+    return (
+        <HomeTemplate>
+            <div className={classnames("ui-looking-glass-wrapper")}>
+                <Console />
+            </div>
+        </HomeTemplate>
+    );
+}
