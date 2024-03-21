8c65a9d6ea2aa1e17676552d283b53ec95f7b56b
bramirez96 <bran.ramirez.don@gmail.com>
2024-03-21 12:24:26 -0700

    No more game callback object type needed. The GameController itself now holds all of that data, and is passed as the final argument of every game callback.

--- src/libs/through-the-looking-glass-v1.0.0/src/gameTypes.ts
+++ src/libs/through-the-looking-glass-v1.0.0/src/gameTypes.ts
@@ -1,23 +1,7 @@
 // ! Copyright (c) 2024, Brandon Ramirez, brr.dev
 
-import GameController from "./GameController";
-import Player from "./classes/Player";
-import Zone, { ZoneDefinition } from "./classes/Zone";
-import { ReactNode } from "react";
+import { ZoneDefinition } from "./classes/Zone";
 import { ZoneID } from "../../through-the-looking-glass-v0.1.0/src/GameController";
-
-/**
- * Every callback, no matter what it relates to, could possibly require access
- * to the Game, Player, and Zone objects. This object should get passed as the
- * final argument to every game callback we define.
- *
- * TODO remove IOType default eventually, and pass it along as needed
- */
-export type GameCallbackState<IOType = ReactNode> = {
-    game: GameController<IOType>;
-    zone: Zone;
-    player: Player;
-};
 
 /**
  * Conditions should be tracked by creating string constants for reference keys
