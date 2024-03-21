b2ef9a14d71455d03ad5ad8e8a850dfce8f90172
bramirez96 <bran.ramirez.don@gmail.com>
2024-03-21 12:11:41 -0700

    New "asFunction" helper that uses type inference to greatly reduce boilerplate in my interactable classes

--- src/libs/through-the-looking-glass-v1.0.0/src/gameHelpers.ts
+++ src/libs/through-the-looking-glass-v1.0.0/src/gameHelpers.ts
@@ -1,6 +1,13 @@
 // ! Copyright (c) 2024, Brandon Ramirez, brr.dev
 
 /**
+ * Return the value if it's a function, or return a new cb returning the value.
+ */
+export function asFunction<CBType, DataType>(data: DataType) {
+    return (typeof data !== "function" ? () => data : data) as CBType;
+}
+
+/**
  * Format the input string as an interactive tag. Instead of calling this directly,
  * it's recommended to use the `tag` method below!
  */
