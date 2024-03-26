8337cb7dbe23e83f375aac09dc45223f924759dc
bramirez96 <bran.ramirez.don@gmail.com>
2024-03-26 15:39:50 -0700

    Add inventory to Player class

--- src/libs/through-the-looking-glass-v1.0.0/src/classes/Player.ts
+++ src/libs/through-the-looking-glass-v1.0.0/src/classes/Player.ts
@@ -1,14 +1,19 @@
 // ! Copyright (c) 2024, Brandon Ramirez, brr.dev
 
 import { ConditionMap } from "../gameTypes";
+import Item from "./Item";
 
 export const PLAYER_CONDITIONS = {
     AGORAPHOBIC: "agoraphobic",
 };
 
 export default class Player {
+    private readonly inventory: Item[] = [];
+
     private readonly conditions: ConditionMap = {
         // Player is initialized with agoraphobia by default.
+        // TODO move this condition to a playerConditions property on the Zone/disc
+        //  so this class can be reused for different games if we wanted
         [PLAYER_CONDITIONS.AGORAPHOBIC]: true,
     };
 
@@ -18,5 +23,9 @@ export default class Player {
 
     setCondition(condition: string, state: boolean): void {
         this.conditions[condition] = state;
+    }
+
+    viewInventory(): void {
+        // TODO print out items
     }
 }
